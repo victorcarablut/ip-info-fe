52eaeefd3520f6b6eb39f950bdb0c8399913c5ea
victorcarablut <victor.carablut@gmail.com>
2024-10-04 12:02:18 +0200

    updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
     <footer>
       <a href="https://github.com/victorcarablut/ip-info-fe" target="_blank" rel="noopener noreferrer" className="git-link">
         <i className="bi bi-github" />
-        <p>GitHub</p>
+        <p>GitHub Repository</p>
       </a>
       <p><small>© {dateYear} All rights reserved.</small></p>
       <a href="https://code.victorcarablut.com" target="_blank" rel="noopener noreferrer"><small>code.victorcarablut.com</small></a>
